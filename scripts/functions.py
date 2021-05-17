4efa6ad99f17d344dd559b0708a507eb62c62ee0
Roberto Canale <robyrugby95@gmail.com>
2021-05-17 12:56:43 +0200

    Initial commit

---
+++ scripts/functions.py
@@ -0,0 +1,75 @@
+import numpy as np
+import argparse
+import torch
+import cv2
+import time
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized
+
+#For Raw Detections
+class BoundingBox:
+    label = ""
+    confidence = 0.0
+    coordinates = np.zeros(4, dtype=np.float32)
+    id = 0
+    def __init__(self):
+        pass
+    def set_bb(self, id, label, confidence, coordinates):
+        self.id = id
+        self.label = label
+        self.confidence = confidence
+        self.coordinates = coordinates
+
+#For Reshaped Detections
+class BoundingBoxReshaped:
+    label = ""
+    confidence = 0.0
+    coordinates_reshaped = np.zeros(4, dtype=np.int32)
+    id = 0
+    def __init__(self):
+        pass
+    def set_bb(self, id, label, confidence, coordinates_reshaped):
+        self.id = id
+        self.label = label
+        self.confidence = confidence
+        self.coordinates_reshaped = coordinates_reshaped
+
+def reshape_coordinates_bb (coord_in, width_i, height_i, width_o, height_o):
+    coord_out = np.zeros(len(coord_in), dtype= np.int32)
+    for i in range(len(coord_in)):
+        coord_out[0] = int(max(float(0), coord_in[0]*width_o/width_i))  #x1
+        coord_out[1] = int(max(float(0), coord_in[1]*height_o/height_i))   #y1
+        coord_out[2] = int(max(float(0), coord_in[2]*width_o/width_i))  #x2
+        coord_out[3] = int(max(float(0), coord_in[3]*height_o/height_i)) #y2
+    return coord_out
+
+def bounding_box_predictions(det, bb_number, names):
+    bb_predictions = [BoundingBox() for i in range(bb_number)]
+        #bb = "BOUNDING BOXES:" + str(round(time.time(), 5)) + " " #string to write on
+    for i in range(bb_number): #scan the prediction matrix DET/PRED (they are the same)
+        coordinates=[round(det[i][0].item(),3),round(det[i][1].item(),3),round(det[i][2].item(),3),round(det[i][3].item(),3)] 
+        confidence = round(det[i][4].item(),5)
+        obj_class_id = int(det[i][5].item())
+        obj_class = names[int(det[i][5].item())]
+        bb_predictions[i].set_bb(obj_class_id, obj_class, confidence, coordinates)
+        print(bb_predictions[i].label,bb_predictions[i].confidence,bb_predictions[i].coordinates )
+
+    return bb_predictions
+
+def bounding_box_predictions_reshaped(bb_predictions, bb_number, I_backtorgb, colors, rows, cols):
+    bb_predictions_reshaped = [BoundingBoxReshaped() for i in range(bb_number)]
+    for i in range(bb_number): 
+        xyxy = reshape_coordinates_bb(bb_predictions[i].coordinates, 416, 416, rows, cols) #for a different image size
+        bb_predictions_reshaped[i].set_bb(bb_predictions[i].id, bb_predictions[i].label, bb_predictions[i].confidence, xyxy)
+        print("Coordintes Reshaped ", xyxy)
+
+    for i in range(bb_number):  #reshaped detections on image
+            label = str(bb_predictions_reshaped[i].label) + " " + str(round(bb_predictions_reshaped[i].confidence, 2))
+            plot_one_box(bb_predictions_reshaped[i].coordinates_reshaped, I_backtorgb, label=label, color=colors[bb_predictions[i].id], line_thickness=1) 
+
+    return bb_predictions_reshaped, I_backtorgb
+        
